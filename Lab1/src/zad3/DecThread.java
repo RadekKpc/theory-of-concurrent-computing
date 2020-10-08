d5363bd4dcb1a2cfc91bd7fce897f43266819c7e
RadekKpc <radek4ec@gmail.com>
2020-10-08 19:33:07 +0200

    Remove comments

--- Lab1/src/zad3/DecThread.java
+++ Lab1/src/zad3/DecThread.java
@@ -9,22 +9,6 @@ public class DecThread extends Thread {
         this.threadManager = threadManager;
     }
 
-//    @Override
-//    public void run(){
-//        for(int i=0;i<10;){
-//            threadManager.setFirst_ready(true);
-//
-//            if(threadManager.isFirstIn()){
-//                i++;
-//                threadManager.getCounter().dec();
-//                threadManager.setFirst_ready(false);
-//                threadManager.setFirstIn(false);
-//                System.out.println("First work done");
-//            }
-//        }
-//        threadManager.setFirstFisihedWork(true);
-//    }
-
     @Override
     public void run(){
         for(int i=0;i<1000000;){
