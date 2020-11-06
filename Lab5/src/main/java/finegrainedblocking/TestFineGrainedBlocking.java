11034da4947825e62831c381ec70e574a4224b07
RadekKpc <radek4ec@gmail.com>
2020-11-06 20:25:20 +0100

    Lab1 ex2 finiseh, add 2d ploting #3

--- Lab5/src/main/java/finegrainedblocking/TestFineGrainedBlocking.java
+++ Lab5/src/main/java/finegrainedblocking/TestFineGrainedBlocking.java
@@ -11,11 +11,13 @@ import org.jzy3d.plot3d.builder.Mapper;
 import org.jzy3d.plot3d.builder.concrete.OrthonormalGrid;
 import org.jzy3d.plot3d.primitives.Shape;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
+import org.math.plot.Plot2DPanel;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-
 import static java.lang.Math.max;
+import static java.lang.Math.min;
 
 public class TestFineGrainedBlocking {
 
@@ -24,47 +26,104 @@ public class TestFineGrainedBlocking {
 //        za zasadne uznam zbadanie wydajności list w zależności nie tylko od kosztu ale również od ilośći wątków obsługujących ta listę, gdyż dla jedengo wątku nie ma to sensu,
 //        zajmuje on po porstu cała kolejke, przewaga kolejni z wieloma blokadami polega włąsśnie na wielodostepie zatem zbadam tez w zaleznosci od roznej liczby wątków.
 
+//        Global Parameters
         final int operations = 1;
-        final int minCost = 1;
-        final int maxCost = 1;
-        final int costStep = 1;
-        final int minExecutors = 100;
-        final int maxExecutors = 100;
 
+//       Cost measurement parameters
+        final int minCost = 1;
+        final int maxCost = 100;
+        final int costStep = 1;
+        final int threadsForCostMeasurement = 5;
         final int countOfCost = maxCost - minCost + 1;
+
+//       Threads measurement parameters
+
+        final int minExecutors = 1;
+        final int maxExecutors = 30;
+        final int costForThreadMeasurement = 10;
         final int countOfExecutors = maxExecutors - minExecutors + 1;
 
-        long[][] oneLock = new long[countOfCost][countOfExecutors];
-        long[][] multipleLocks = new long[countOfCost][countOfExecutors];
+//        Visualization
 
-        for(int cost=minCost;cost<=maxCost;cost+=costStep){
-            for(int ex =minExecutors; ex <= maxExecutors; ex++) {
+        double[] oneLockCost = new double[countOfCost];
+        double[] multipleLocksCost = new double[countOfCost];
+        double[] xCostAxis = new double[countOfCost];
 
-                ArrayList<Thread> executorsOneLock = new ArrayList<>();
-                ArrayList<Thread> executorsMultipleLocks = new ArrayList<>();
+        double[] oneLockThreads = new double[countOfExecutors];
+        double[] multipleLocksThreads = new double[countOfExecutors];
+        double[] xThreadsAxis = new double[countOfExecutors];
 
-                NodeList oneLockList = new NodeListWithOneLock(cost);
-                NodeList multipleLocksList = new NodeListMultipleLocks(cost);
+        double[] oneLockCostOneThread = new double[countOfCost];
+        double[] multipleLocksCostOneThread = new double[countOfCost];
+        double[] xConstOneThreadAxis = new double[countOfCost];
 
-                for (int i = 0; i < ex; i++) {
-                    executorsOneLock.add(new NodeListExecutor(oneLockList, operations));
-                    executorsMultipleLocks.add(new NodeListExecutor(multipleLocksList, operations));
-                }
+//        Measurement the time for multiple Threads and const cost
 
-                oneLock[cost - minCost][ex - minExecutors] = measureTime(executorsOneLock);
-                multipleLocks[cost - minCost][ex - minExecutors] = measureTime(executorsMultipleLocks);
+        for(int cost=minCost;cost<=maxCost;cost+=costStep) {
+
+            NodeList oneLockList = new NodeListWithOneLock(cost);
+            NodeList multipleLocksList = new NodeListMultipleLocks(cost);
+
+            ArrayList<Thread> executorsOneLock = new ArrayList<>();
+            ArrayList<Thread> executorsMultipleLocks = new ArrayList<>();
+
+            for (int i = 0; i < threadsForCostMeasurement; i++) {
+                executorsOneLock.add(new NodeListExecutor(oneLockList, operations));
+                executorsMultipleLocks.add(new NodeListExecutor(multipleLocksList, operations));
             }
+
+            oneLockCost[cost - minCost] = (double) measureTime(executorsOneLock);
+            multipleLocksCost[cost - minCost] = (double) measureTime(executorsMultipleLocks);
+            xCostAxis[cost - minCost] = cost;
+            System.out.println("Cost Measurement: " +  ((double) cost/maxCost) * 100.0 + "%");
+        }
+
+//        Measurement the time for multiple costs and const count of threads
+        System.out.println("Threads measurement:");
+        for(int ex =minExecutors; ex <= maxExecutors; ex++) {
+
+            NodeList oneLockList = new NodeListWithOneLock(costForThreadMeasurement);
+            NodeList multipleLocksList = new NodeListMultipleLocks(costForThreadMeasurement);
+
+            ArrayList<Thread> executorsOneLock = new ArrayList<>();
+            ArrayList<Thread> executorsMultipleLocks = new ArrayList<>();
+
+            for (int i = 0; i < ex; i++) {
+                executorsOneLock.add(new NodeListExecutor(oneLockList, operations));
+                executorsMultipleLocks.add(new NodeListExecutor(multipleLocksList, operations));
+            }
+
+            oneLockThreads[ex - minExecutors] = (double) measureTime(executorsOneLock);
+            multipleLocksCostOneThread[ex - minExecutors] = (double) measureTime(executorsMultipleLocks);
+            xThreadsAxis[ex - minExecutors] = ex;
+
+            System.out.println("Threads Measurement: " + ((double) ex/maxExecutors) * 100 + "%");
 
         }
 
-        printResult(oneLock,countOfCost,countOfExecutors);
-        printResult(multipleLocks,countOfCost,countOfExecutors);
+//        Measurement the time for multiple costs and one thread
 
-        calculateAverage(oneLock,countOfCost,countOfExecutors,"One Lock");
-        calculateAverage(multipleLocks,countOfCost,countOfExecutors,"Multiple Lock");
+        for(int cost=minCost;cost<=maxCost;cost+=costStep) {
 
-        create3dPlot(oneLock,countOfCost,countOfExecutors,"One Lock \n t = time = f(koszt,liczba_watków)");
-        create3dPlot(multipleLocks,countOfCost,countOfExecutors,"Multiple Lock \n t = time = f(koszt,liczba_watków)");
+            NodeList oneLockList = new NodeListWithOneLock(cost);
+            NodeList multipleLocksList = new NodeListMultipleLocks(cost);
+
+            ArrayList<Thread> executorsOneLock = new ArrayList<>();
+            ArrayList<Thread> executorsMultipleLocks = new ArrayList<>();
+
+            executorsOneLock.add(new NodeListExecutor(oneLockList, operations));
+            executorsMultipleLocks.add(new NodeListExecutor(multipleLocksList, operations));
+
+            oneLockCostOneThread[cost - minCost] = (double) measureTime(executorsOneLock);
+            multipleLocksCostOneThread[cost - minCost] = (double) measureTime(executorsMultipleLocks);
+            xConstOneThreadAxis[cost - minCost] = cost;
+            System.out.println("Cost Measurement one thread: " + ((double) cost/maxCost) * 100 + "%");
+        }
+//        visualization
+
+        draw2DPlots(xCostAxis,oneLockCost,xCostAxis,multipleLocksCost,"Lock na całą listę","Lock na każdy element listy","Porównanie zalezności czasu od kosztu");
+        draw2DPlots(xThreadsAxis,oneLockThreads,xThreadsAxis,multipleLocksThreads,"Lock na całą listę","Lock na każdy element listy","Porównanie zalezności czasu od ilosci wątków");
+        draw2DPlots(xConstOneThreadAxis,oneLockCostOneThread,xConstOneThreadAxis,multipleLocksCostOneThread,"Lock na całą listę","Lock na każdy element listy","Porównanie zalezności czasu od ksoztu dla jedengo wątku");
 
     }
     private static void printResult(long[][] table, int X,int Y){
@@ -126,6 +185,31 @@ public class TestFineGrainedBlocking {
     private static void calculateAverage(long[][] tab, int X,int Y, String info){
         long sum = Arrays.stream(tab).flatMapToLong(Arrays::stream).reduce(0, Long::sum);
         System.out.println("Average for" + info + ": " + (double) sum/(1000000*(X*Y)) + "ms");
+    }
+
+    private static void draw2DPlot(double[] arguments, double[] values){
+
+        Plot2DPanel plot = new Plot2DPanel();
+        // add a line plot to the PlotPanel
+        plot.addLinePlot("my plot", arguments, values);
+
+        // put the PlotPanel in a JFrame, as a JPanel
+        JFrame frame = new JFrame("a plot panel");
+        frame.setContentPane(plot);
+        frame.setVisible(true);
+    }
+
+    private static void draw2DPlots(double[] arguments, double[] values, double[] arguments2, double[] values2, String name, String name2, String plotName){
+
+        Plot2DPanel plot = new Plot2DPanel();
+        // add a line plot to the PlotPanel
+        plot.addLinePlot(name, arguments, values);
+        plot.addLinePlot(name2, arguments2, values2);
+
+        // put the PlotPanel in a JFrame, as a JPanel
+        JFrame frame = new JFrame(plotName);
+        frame.setContentPane(plot);
+        frame.setVisible(true);
     }
 }
 
